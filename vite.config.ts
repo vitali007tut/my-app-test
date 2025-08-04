b692fb57d29ca2d85519b8f1038addb8e4474c51
Vitali <61989351+vitali007tut@users.noreply.github.com>
2025-08-04 21:10:43 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    // base: '/my-app-test/',
-    base: '/',
+    base: '/my-app-test/',
     plugins: [react()],
 });
